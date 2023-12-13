f24a9c32c60efc10c9d4fdfaa136e9a6e5c10c06
Kanhaiyaa <128280779+Kanhaiyaa@users.noreply.github.com>
2023-12-13 07:59:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+
+const counterElement=document.getElementById('counter')
+const incrementBtn=document.getElementById('incrementBtn')
+
+let counterValue=0
+
+function incrementCounter() {
+	alert("un--incremented value: "+counterValue)
+	counterValue++
+
+	counterElement.textContent=counterValue
+}
+incrementBtn.addEventListener('click',incrementCounter)
